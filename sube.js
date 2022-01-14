f8c2434b374038ba59d9ee60402ad6092450d4a3
dy <df.creative@gmail.com>
2022-01-14 16:19:21 -0500

    Remove useless try catch

--- sube.js
+++ sube.js
@@ -13,11 +13,12 @@ export default (target, next, error, complete, stop) =>
   target && (
     target.subscribe?.( next, error, complete ) ||
     target[Symbol.observable]?.().subscribe?.( next, error, complete ) ||
-    target.set && target.call?.(stop, v => { try { next(v) } catch (err) { error?.(err) } }) || // observ
+    target.set && target.call?.(stop, next) || // observ
     (
       target.then?.(v => (!stop && next(v), complete?.()), error) ||
       (async _ => {
         try {
+          // FIXME: possible drawback: it will catch error happened in next, not only in iterator
           for await (target of target) { if (stop) return; next(target) }
           complete?.()
         } catch (err) { error?.(err) }
